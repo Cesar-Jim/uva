49b784ed438a799bc265242f283987f2c230799b
Cesar Jimenez <cijgr@hotmail.com>
2019-03-02 14:08:31 -0600

    Deployment to production and implmeneted port normalization

---
+++ src/config/main-config.js
@@ -0,0 +1,7 @@
+// When dotenv is configured, it will look for .env in the root and load environment variables into memory.
+require("dotenv").config();
+
+// Configuration object that will configure all middleware:
+module.exports = {
+  init() {}
+};
