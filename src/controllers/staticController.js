5f2ce324735768153127ea06f9658a386bf23ba3
Cesar Jimenez <cijgr@hotmail.com>
2019-03-03 00:05:02 -0600

    Added about us page and it's integration spec

--- src/controllers/staticController.js
+++ src/controllers/staticController.js
@@ -4,5 +4,8 @@ module.exports = {
   index(req, res, next) {
     res.render("static/index", { title: "Welcome to uva" });
     // render() takes the location of the template and the object containing the data passed in the template
+  },
+  about(req, res, next) {
+    res.render("static/about-us", { title: "About Us" });
   }
 };
--- src/routes/static.js
+++ src/routes/static.js
@@ -5,5 +5,6 @@ const staticController = require("../controllers/staticController");
 
 // Defining the route "/". Set the root path to accept a GET request.
 router.get("/", staticController.index);
+router.get("/about", staticController.about);
 
 module.exports = router;
